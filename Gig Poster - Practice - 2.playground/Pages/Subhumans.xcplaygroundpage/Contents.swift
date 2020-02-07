f9c8db6ba76d41d5515ea0158cf9a13c57adbd7e
Branton Hung <BHung@lcs.on.ca>
2020-02-07 11:28:12 -0500

    Finished

--- Gig Poster - Practice - 2.playground/Pages/Subhumans.xcplaygroundpage/Contents.swift
+++ Gig Poster - Practice - 2.playground/Pages/Subhumans.xcplaygroundpage/Contents.swift
@@ -34,7 +34,78 @@ let lightRed = Color(hue: 10, saturation: 80, brightness: 80, alpha: 75)
 
 // Begin your solution here...
 
+//Draw the background
+canvas.fillColor = yellow
+canvas.drawRectangle(at: Point(x: 0, y: 0), width: 400, height: 600)
 
+//Make the arrays for the parrarelagram
+
+for xVal in stride(from: 15, through: 330, by: 315) {
+    for yVal in stride(from: 50, through: 325, by: 175){
+var shapePoints : [Point] = []
+shapePoints.append (Point(x: xVal, y: yVal))
+shapePoints.append (Point(x: 50+xVal, y: 100+yVal))
+shapePoints.append (Point(x: 200+xVal, y: 100+yVal))
+shapePoints.append (Point(x: 150+xVal, y: yVal))
+    //Draw the pararellegrams
+    canvas.fillColor = purple
+    canvas.drawCustomShape(with: shapePoints)
+    }
+}
+
+for xVal in stride(from:-142 , through: 173, by: 315) {
+    for yVal in stride(from: 50, through: 325, by: 175){
+var shapePoints : [Point] = []
+shapePoints.append (Point(x: xVal, y: yVal))
+shapePoints.append (Point(x: 50+xVal, y: 100+yVal))
+shapePoints.append (Point(x: 200+xVal, y: 100+yVal))
+shapePoints.append (Point(x: 150+xVal, y: yVal))
+    //Draw the pararellegrams
+    canvas.fillColor = brown
+        canvas.drawShapesWithBorders = false
+    canvas.drawCustomShape(with: shapePoints)
+    }
+}
+
+for xVal in stride(from:-142 , through: 473, by: 157) {
+var shapePoints : [Point] = []
+shapePoints.append (Point(x: xVal, y: 140))
+shapePoints.append (Point(x: 50+xVal, y: 240))
+shapePoints.append (Point(x: 200+xVal, y: 240))
+shapePoints.append (Point(x: 150+xVal, y: 140))
+    //Draw the pararellegrams
+    canvas.fillColor = lightRed
+    canvas.drawShapesWithBorders = false
+    canvas.drawCustomShape(with: shapePoints)
+    }
+
+//Draw the texts
+canvas.drawText(message: "subhumans", at: Point(x: 10, y: 325), size: 48, kerning: 0)
+
+canvas.drawText(message: "no age limit", at: Point(x: 15, y: 396), size: 10, kerning: 0)
+canvas.drawText(message: "advance $6.50, door $7.50", at: Point(x: 15, y: 411), size: 10, kerning: 0)
+canvas.drawText(message: "friday, april 13 1984", at: Point(x: 15, y: 426), size: 10, kerning: 0)
+
+canvas.drawText(message: "los angeles, california", at: Point(x: 150, y: 396), size: 10, kerning: 0)
+canvas.drawText(message: "1801 s. grand ave.", at: Point(x: 150, y: 411), size: 10, kerning: 0)
+canvas.drawText(message: "at olypic auditorium", at: Point(x: 150, y: 426), size: 10, kerning: 0)
+
+canvas.drawText(message: "new regime & red scare", at: Point(x: 275, y: 396), size: 10, kerning: 0)
+canvas.drawText(message: "milti death corporations", at: Point(x: 275, y: 411), size: 10, kerning: 0)
+canvas.drawText(message: "with the tourists", at: Point(x: 275, y: 426), size: 10, kerning: 0)
+
+
+
+//draw a grid for refrence
+for y in stride(from: 0, through: 600, by: 50) {
+    //Draw the grid lines
+    canvas.drawLine(from: Point(x: 0, y: y), to: Point(x: 400, y: y))
+}
+
+for x in stride(from: 0, through: 400, by: 50) {
+    //Draw the grid line
+    canvas.drawLine(from: Point(x: x, y: 600), to: Point(x: x, y: 0))
+}
 /*:
  ## Use Source Control
  
@@ -46,4 +117,5 @@ let lightRed = Color(hue: 10, saturation: 80, brightness: 80, alpha: 75)
  The code below is necessary to see results in the Assistant Editor at right. Please do not remove.
  */
 PlaygroundPage.current.liveView = canvas
+
 
